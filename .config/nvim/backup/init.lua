844022385bc74071990ee87011b5a714760faca0
Shubham Bhakuni <shubhambhakuni26@gmail.com>
2022-06-19 12:02:57 +0530

    Added WM configs

---
+++ .config/nvim/backup/init.lua
@@ -0,0 +1,11 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp" -- auto completion
+require "user.lsp"
+
+-- Enable colourscheme
+vim.cmd "colorscheme aurora"
+
+
---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp" -- auto completion
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+
+-- Enable colourscheme
+vim.cmd "colorscheme darkplus"
+
+
